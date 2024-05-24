121976c36b7b5715a98e969c92ab428b1d16ea2a
吳超秋 <wuchaoqiu@wuchaoqiudeMacBook-Air.local>
2024-05-24 15:49:53 +0800

    Add first test spec

--- test/__helpers__/mongo.helper.ts
+++ test/__helpers__/mongo.helper.ts
@@ -6,10 +6,16 @@ export class MongoHelper extends CustomMongoClient {
       minPoolSize: Number.parseInt(process.env.DEFAULT_MONGO_MIN_POOL),
       maxPoolSize: Number.parseInt(process.env.DEFAULT_MONGO_MAX_POOL),
       user: process.env.DEFAULT_MONGO_USER,
-      pass: process.env.DEFAULT_MONGO_URI,
-      db: process.env.DEFAULT_MONGO_PASS,
+      pass: process.env.DEFAULT_MONGO_PASS,
+      db: process.env.DEFAULT_MONGO_DB_NAME,
       directConnect: true,
       connectTimeoutMS: Number.parseInt(process.env.DEFAULT_MONGO_CONN_TIMEOUT),
     });
+  }
+
+  public async clear(cols: string[]): Promise<void> {
+    for (const col of cols) {
+      await this.getCollection(col).deleteMany({});
+    }
   }
 }
---
+++ test/post-v1-clients.e2e-spec.ts
@@ -0,0 +1,60 @@
+import * as _ from 'lodash';
+import * as superTest from 'supertest';
+import { AppHelper } from './__helpers__/app.helper';
+import { MongoHelper } from './__helpers__/mongo.helper';
+
+interface IBody {
+    name: string;
+    callbackUrl: string;
+};
+
+describe(`POST ${process.env.DEFAULT_API_ROUTER_PREFIX}/v1/clients spec`, () => {
+    const endpoint = `${process.env.DEFAULT_API_ROUTER_PREFIX}/api/v1/clients`;
+    let agent: superTest.SuperAgentTest;
+    const db = new MongoHelper();
+    const clientCol = 'Clients';
+    const defaultBody: IBody = {
+        name: 'iLearning',
+        callbackUrl: 'https://xxx.ccc.com',
+    };
+    beforeAll(async () => {
+        agent = await AppHelper.getAgent();
+        await db.tryConnect();
+    });
+    afterAll(async () => {
+        await AppHelper.closeAgent();
+        await db.clear([clientCol]);
+        db.close();
+    });
+    describe('Required fields', () => {
+        test('[10001] Parameter "name" is empty', async () => {
+            const b = _.cloneDeep(defaultBody);
+            b.name = '';
+            const res = await agent
+                .post(endpoint)
+                .send(b);
+
+            expect(res.status).toBe(400);
+            expect(res.body.code).toBe(10001);
+            expect(res.body.message).toBe('無效客戶名稱');
+        });
+        test('[10002] Parameter "callbackUrl" is empty', async () => {
+            const b = _.cloneDeep(defaultBody);
+            b.callbackUrl = '';
+            const res = await agent
+                .post(endpoint)
+                .send(b);
+
+            expect(res.status).toBe(400);
+            expect(res.body.code).toBe(10002);
+            expect(res.body.message).toBe('無效客戶回調網址');
+        });
+    });
+    describe('Validation rules', () => {
+
+    });
+    describe('Success', () => {
+
+    });
+
+});
