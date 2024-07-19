da5c8e82e8491a9804f94349178cfe92c3b9f70c
xxxhand <nohandboy@gmail.com>
2024-07-19 14:29:21 +0800

    Add deleting uploaded files after catching exception

--- src/app-components/app-exception.filter.ts
+++ src/app-components/app-exception.filter.ts
@@ -1,9 +1,7 @@
+import * as fs from 'fs-extra';
 import { Request, Response } from 'express';
 import { ErrException, CommonService } from '@myapp/common';
-import { CustomResult } from '@xxxhand/app-common';
 import { ExceptionFilter, Catch, ArgumentsHost, Logger } from '@nestjs/common';
-
-import * as appConstants from '@myapp/common';
 
 @Catch()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -20,5 +18,18 @@ export class AppExceptionFilter implements ExceptionFilter {
 
     if (res.headersSent) return;
     res.status(err.getStatus()).json(this.cmmService.newResultInstance().withCode(err.getCode()).withMessage(err.getMessage()));
+    // Delete uploaded files
+    const tasks = []
+    if (req.file) {
+      tasks.push(fs.unlink(req.file.path));
+    }
+    if (req.files) {
+      if (Array.isArray(req.files)) {
+        req.files.forEach((x) => tasks.push(fs.unlink(x.path)));
+      } else {
+        Object.keys(req.files).forEach((k: string) => (req.files[k] as Express.Multer.File[]).forEach((x: Express.Multer.File) => tasks.push(fs.unlink(x.path))));
+      }
+    }
+    Promise.all(tasks).catch((ex) => Logger.warn(ex));    
   }
 }
